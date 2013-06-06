707be889a4a626a2a9d6ba0296430ecf7a27cc10
hemmer <ewan.hemingway@gmail.com>
2013-06-06 22:48:08 +0100

    Updated Jucer file

--- JuceLibraryCode/modules/juce_audio_utils/juce_audio_utils.h
+++ JuceLibraryCode/modules/juce_audio_utils/juce_audio_utils.h
@@ -1,5 +1,13 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../modules/juce_audio_utils/juce_audio_utils.h"
+#if defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "../../../../../modules/juce_audio_utils/juce_audio_utils.h"
+#elif defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../../../modules/juce_audio_utils/juce_audio_utils.h"
+#elif defined (JUCER_VS2012_78A501F)
+ #include "C:/SDKs/juce/modules/juce_audio_utils/juce_audio_utils.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
 
--- JuceLibraryCode/modules/juce_gui_extra/juce_gui_extra.h
+++ JuceLibraryCode/modules/juce_gui_extra/juce_gui_extra.h
@@ -1,5 +1,13 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../modules/juce_gui_extra/juce_gui_extra.h"
+#if defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "../../../../../modules/juce_gui_extra/juce_gui_extra.h"
+#elif defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../../../modules/juce_gui_extra/juce_gui_extra.h"
+#elif defined (JUCER_VS2012_78A501F)
+ #include "C:/SDKs/juce/modules/juce_gui_extra/juce_gui_extra.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
 
